fb660bd4ead22567461843692c85e16a9c53c536
Александр Ягупьев <siaghupiev@mail.ru>
2024-08-13 21:10:39 +0500

    Created classes and added a test option for reading csv

--- Program.cs
+++ Program.cs
@@ -1,10 +1,30 @@
-﻿namespace LoadingTextFileToDatabase
+﻿using System.IO;
+
+namespace LoadingTextFileToDatabase
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Console.WriteLine("Укажите путь к файлу формата csv");
+                string path = Console.ReadLine();
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string text = sr.ReadToEnd().Replace(Environment.NewLine, " ");
+                    string[] split = text.Split(';');
+                    foreach (string line in split)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+                Console.ReadKey();
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
